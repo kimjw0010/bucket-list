c215041e70151cb518b123cbf09516f9ff688767
mihyun <mihy621@gmail.com>
2022-05-01 21:41:45 +0900

    chore: Tailwind CSS환경 설정

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./src/main/resources/static/**/*.{js,jsx,ts,tsx}",
+    "./src/main/resources/templates/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
